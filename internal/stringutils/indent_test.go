9d36b8ed256235d57b8f8e4b4b4eb1c43ca8aba1
Martin Ohmann <martin.ohmann@bonial.com>
2024-11-27 11:15:43 +0100

    feat: basic CLI implementation

---
+++ internal/stringutils/indent_test.go
@@ -0,0 +1,46 @@
+package stringutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndent(t *testing.T) {
+	t.Run("empty string", func(t *testing.T) {
+		assert.Equal(t, "", Indent("", 2, true))
+	})
+
+	t.Run("multiline string", func(t *testing.T) {
+		given := `foo
+bar
+
+  baz`
+		expected := `  foo
+  bar
+
+    baz`
+
+		assert.Equal(t, expected, Indent(given, 2, true))
+	})
+
+	t.Run("skip indent first", func(t *testing.T) {
+		given := `foo
+bar
+
+  baz`
+		expected := `foo
+  bar
+
+    baz`
+
+		assert.Equal(t, expected, Indent(given, 2, false))
+	})
+
+	t.Run("trailing newline", func(t *testing.T) {
+		given := "foo\nbar\n"
+		expected := "  foo\n  bar\n"
+
+		assert.Equal(t, expected, Indent(given, 2, true))
+	})
+}
